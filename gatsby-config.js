91c46fdf62876e305f74db3d6c44a1cf4168478e
christien-kelly <christien.kelly@gmail.com>
2023-03-04 13:29:48 -0500

    hid the gtag plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,20 @@ module.exports = {
     title: `christien`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    //"gatsby-plugin-google-gtag", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+]};
